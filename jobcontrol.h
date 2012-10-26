f542197968fa222b832462c0b4496c618d2c00ac
root <root@bt.foo.org>
2012-10-26 00:19:28 -0400

    More job control stuff

--- jobcontrol.h
+++ jobcontrol.h
@@ -20,14 +20,52 @@ void showJobs(){
 t_job* getJob(int val, int par){
 	t_job* jobs = jobList;
 	if(par==1){
-		// Find by pID
-	} else if(par==2){
 		// Find by jID
+		while(jobs!=NULL){
+			if(jobs->id == val){
+				return jobs;
+			} else {
+				jobs = jobs->next;
+			}
+	} else if(par==2){
+		// Find by pID
+		while(jobs!=NULL){
+			if(jobs->pid == val){
+				return jobs;
+			} else {
+				return jobs = jobs->next;
+			}
+		}
 	} else if(par==3){
 		// Find by stat
+		while(jobs!=NULL){
+			if(jobs->status == val){
+				return jobs;
+			} else {
+				jobs = jobs->next;
+			}
+		}
 	} else {
 		// Nothing set
 		return NULL;
 		break;
 	}
 }
+
+t_job* delJob(t_job* checkJob){
+	t_job* curJob;
+	t_job* prevJob;
+	if(jobList==NULL){
+		return NULL;
+	}
+	curJob=jobList->next;
+	prevJob=jobList;
+	if(prevJob->pid == checkJob->pid){
+		prevJob = prevJob->next;
+		activeJobs--;
+		return curJob;
+	}
+	// HERE
+}
+
+
